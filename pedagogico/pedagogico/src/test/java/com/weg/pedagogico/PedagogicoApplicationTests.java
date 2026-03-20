f00f3eee580ecfb5b19c095b671cca8f134136b4
Vinicim <vinicius_anacleto@estudante.sesisenai.org.br>
2026-03-20 18:39:48 -0300

    Add files via upload

---
+++ pedagogico/pedagogico/src/test/java/com/weg/pedagogico/PedagogicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.pedagogico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedagogicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
